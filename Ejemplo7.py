0dcd8a4090950eddb611554f8a7829025306d679
esteban garcia <esteban.garciag@uqvirtual.edu.co>
2022-02-15 09:16:11 -0500

    Mi Segundo Repos

---
+++ Ejemplo7.py
@@ -0,0 +1,18 @@
+x=1
+sumar=0
+"""while x<=10:
+    numero=int(input("ingrese un numero: "))
+    suma= suma + numero
+    x= x+1
+print("la suma de los 10 numeros es", suma)"""
+
+for x in range(10):
+    numero = int(input("ingrese un numero: "))
+    sumar = sumar+numero
+print(sumar)
+    
+
+
+    
+
+
